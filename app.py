c32814726590a553097a2e070cea6d8ceee11039
Brian Elizondo <brian_elizondo@msn.com>
2022-05-15 15:15:16 -0400

    Part One: Cupcake Model => Create Cupcake model in models.py, make a database called cupcakes andrun the seed.py file

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 """Flask app for Cupcakes"""
+from crypt import methods
+from flask import Flask, request, redirect, render_template, flash
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "abcd1234"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,22 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_CUPCAKE_IMAGE = "https://thestayathomechef.com/wp-content/uploads/2017/12/Most-Amazing-Chocolate-Cupcakes-1-small.jpg"
+
+def connect_db(app):
+    """Connect to database"""
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Create a Cupcake model"""
+
+    __tablename__ = "cupcake"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_CUPCAKE_IMAGE)
